c4fb435699b5b847fce77548948698a99b36dd12
Sheeves <d.swierzy99@gmail.com>
2018-10-27 03:29:24 +0200

    Update problem001.js

---
+++ problem001.js
@@ -0,0 +1,10 @@
+﻿function triangleArea(a,b,c)
+{if((a>0)&&(b>0)&&(c>0)&&((a+b)>c)&&((a+c)>b)&&((b+c)>a))
+  {
+    var p=(a+b+c)/2;
+    var pole=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    pole=Math.round(pole*100)/100;
+    return pole;
+    
+  }
+else return -1;}
